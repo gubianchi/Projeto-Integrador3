b90a2ae5e31cf9ab99f00025d337a19f182a4d02
gubianchi <gu_vbianchi@hotmail.com>
2024-05-02 14:31:35 -0300

    testando

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 
     implementation("com.google.firebase:firebase-bom:32.3.1")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
-    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.1")
 
     implementation ("androidx.navigation:navigation-fragment-ktx:2.4.2")
     implementation ("androidx.navigation:navigation-ui-ktx:2.4.2")
